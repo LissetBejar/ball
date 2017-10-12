e6ad8cc5256d4dc05695c5119f73995e4f729a05
LissetBejar <32747383+LissetBejar@users.noreply.github.com>
2017-10-12 11:13:41 -0700

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,30 @@
+
+function Ball(){
+  // properties
+  this.clr = color (random (200),random (200),random (200));
+  this.x = random(width);
+  this.y = random(height);
+  this.speedX = random(5, 8);
+  this.speedY = random(5, 8);
+  this.rad = random(20, 40);
+  
+  // methods
+  this.update = function(){
+    this.x = this.x + this.speedX;
+    this.y = this.y + this.speedY;
+    this.checkEdges(); // function CALL
+    this.render();
+  }
+  
+  this.render = function(){
+    fill(this.clr);
+    ellipse(this.x, this.y, this.rad, this.rad);
+  }
+  
+  this.checkEdges = function(){
+    if(this.x > width)  this.speedX = -this.speedX;
+    if(this.x < 0)  this.speedX = -this.speedX;
+    if(this.y > height)  this.speedY = -this.speedY;
+    if(this.y < 0)  this.speedY = -this.speedY;
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,22 @@
+var balls = [];
+
+function setup() {
+ var cnv=createCanvas(1000, 900);
+ var x=(windowWidth-width)/2
+ var y=30
+ cnv.position(x,y);
+ for(var i = 0; i<30; i++){
+   balls.push (new Ball());
+ }
+}
+
+function draw(){
+  fill(20, 50, 220, 10);
+  rect(0, 0, width, height);
+  for(var i = 0; i<balls.length; i++){
+  balls[i].update();
+  }
+  
+  
+}
+
